99199bbdc1b1752d6dc2dedd0ed2ee0ed0552f6f
grzegorz <grzegorzklimek77@gmail.com>
2025-01-20 00:33:59 +0100

    Cały algorytm

--- algorytm_genetyczny.py
+++ algorytm_genetyczny.py
@@ -3,21 +3,34 @@ import pprint
 from typing import Callable, Dict, List, Tuple
 
 
-def krzyzowanie_jednopunktowe(chromosom1: List[int], chromosom2: List[int]) -> List[int]: 
-    if len(chromosom1) != len(chromosom2):
-        raise ValueError("Chromosomy muszą mieć taką samą długość")
-    dlugosc = len(chromosom1)
-    punkt_podzialu = dlugosc // 2
-    return chromosom1[:punkt_podzialu] + chromosom2[punkt_podzialu:]
+def krzyzowanie_jednopunktowe(populacja: List[List[int]]) -> List[List[int]]: 
+    nowa_populacja = []
+    for i in range(0, len(populacja), 2):
+        chromosom1 = populacja[i]
+        chromosom2 = populacja[i + 1]
+        if len(chromosom1) != len(chromosom2):
+            raise ValueError("Chromosomy muszą mieć taką samą długość")
+        dlugosc = len(chromosom1)
+        punkt_podzialu = dlugosc // 2
+        dziecko1 = chromosom1[:punkt_podzialu] + chromosom2[punkt_podzialu:]
+        dziecko2 = chromosom2[:punkt_podzialu] + chromosom1[punkt_podzialu:]
+        nowa_populacja.extend([dziecko1, dziecko2])
+    return nowa_populacja
 
-def krzyzowanie_jednorodne(chromosom1: List[int], chromosom2: List[int]) -> List[int]:
-    if len(chromosom1) != len(chromosom2):
-        raise ValueError("Chromosomy muszą mieć taką samą długość")
-    potomstwo = []
-    pprint.pprint(list(zip(chromosom1, chromosom2)))
-    for gen1, gen2 in zip(chromosom1, chromosom2):
-        potomstwo.append(random.choice([gen1, gen2]))
-    return potomstwo
+def krzyzowanie_jednorodne(populacja: List[List[int]]) -> List[List[int]]:
+    nowa_populacja = []
+    for i in range(0, len(populacja), 2):
+        chromosom1 = populacja[i]
+        chromosom2 = populacja[i + 1]
+        if len(chromosom1) != len(chromosom2):
+            raise ValueError("Chromosomy muszą mieć taką samą długość")
+        dziecko1 = []
+        dziecko2 = []
+        for gen1, gen2 in zip(chromosom1, chromosom2):
+            dziecko1.append(random.choice([gen1, gen2]))
+            dziecko2.append(random.choice([gen1, gen2]))
+        nowa_populacja.extend([dziecko1, dziecko2])
+    return nowa_populacja
 
 def selekcja_turniejowa(populacja: List[List[int]],
                        wyniki_dostosowania_dict: Dict[Tuple[int], int],
@@ -48,11 +61,15 @@ def selekcja_ruletki(populacja: List[List[int]], wyniki_dostosowania_dict: Dict[
                 break
     return wybrane
 
-def mutacja_punktowa(chromosom: List[List[int]], prawdopodobienstwo_mutacji: float) -> List[int]:
-    for i in range(len(chromosom)):
-        if random.random() < prawdopodobienstwo_mutacji:
-            chromosom[i] = 1 - chromosom[i]  # Zamiana 0 na 1 i odwrotnie
-    return chromosom
+def mutacja_punktowa(populacja: List[List[int]], prawdopodobienstwo_mutacji: float) -> List[List[int]]:
+    nowa_populacja = []
+    for chromosom in populacja:
+        nowy_chromosom = chromosom[:]
+        for i in range(len(nowy_chromosom)):
+            if random.random() < prawdopodobienstwo_mutacji:
+                nowy_chromosom[i] = 1 - nowy_chromosom[i]  # Zamiana 0 na 1 i odwrotnie
+        nowa_populacja.append(nowy_chromosom)
+    return nowa_populacja
 
 class AlgorytmGenetyczny:
     def __init__(self,
@@ -63,7 +80,8 @@ class AlgorytmGenetyczny:
                   maks_pojemnosc: int,
                   krzyzowanie: Callable[[List[int], List[int]], List[int]],
                   selekcja: Callable[[List[List[int]], Dict[Tuple[int], int]], List[List[int]]],
-                  mutacja: Callable[[List[List[int]], float], List[int]]):
+                  mutacja: Callable[[List[List[int]], float], List[int]],
+                  prawdopodobienstwo_mutacji: float = 0.01) -> None:
         self.populacja = self.generuj_poczatkowa_populacje(rozmiar_populacji, liczba_przedmiotow)
         self.wagi = wagi
         self.wartosci = wartosci
@@ -98,18 +116,16 @@ class AlgorytmGenetyczny:
         return suma_wartosci
 
     def przeprowadz_algorytm(self, liczba_generacji: int) -> List[int]:
-        for _ in range(liczba_generacji):
-            nowa_populacja = []
-            for _ in range(len(self.populacja) // 2):
-                rodzic1 = random.choice(self.populacja)
-                rodzic2 = random.choice(self.populacja)
-                dziecko1 = self.krzyzowanie(rodzic1, rodzic2)
-                dziecko2 = self.krzyzowanie(rodzic2, rodzic1)
-                nowa_populacja.extend([dziecko1, dziecko2])
+        for generacja in range(1, liczba_generacji + 1):
             
-            nowa_populacja = [self.mutacja(chromosom, 0.01) for chromosom in nowa_populacja]
+            populacja_selekcja = self.selekcja(self.populacja, self.wyniki_dostosowania_dict)
+            populacja_krzyzowanie = self.krzyzowanie(populacja_selekcja)
+            populacja_mutacja = self.mutacja(populacja_krzyzowanie, 0.01)
+            nowa_populacja = populacja_mutacja
+            
             self.wyniki_dostosowania_dict = {tuple(chromosom): self.funkcja_dostosowania(chromosom, self.wartosci, self.wagi, self.maks_pojemnosc) for chromosom in nowa_populacja}
-            self.populacja = self.selekcja(nowa_populacja, self.wyniki_dostosowania_dict)
+            self.populacja = nowa_populacja
+            najlepszy_chromosom = max(self.populacja, key=lambda chromosom: self.wyniki_dostosowania_dict[tuple(chromosom)])
+            print(f"Generacja { generacja} Najlepszy chromosom: {najlepszy_chromosom}, wartość: {self.wyniki_dostosowania_dict[tuple(najlepszy_chromosom)]}")
         
-        najlepszy_chromosom = max(self.populacja, key=lambda chromosom: self.wyniki_dostosowania_dict[tuple(chromosom)])
         return najlepszy_chromosom
